35ed14c7f4e67d2ee329f843d69d8987f0a69968
Meer Estiyak <mmestiyak@gmail.com>
2021-07-28 01:47:32 -0700

    added the functionality so by completing text line-through shows

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const shoppingForm = document.querySelector(".shopping-form");
-const list = document.querySelector(".list");
-const listItemDeleteIcon = document.querySelector(".list__item__delete");
+const shoppingForm = document.querySelector('.shopping-form');
+const list = document.querySelector('.list');
+const listItemDeleteIcon = document.querySelector('.list__item__delete');
 let state = [];
 
-shoppingForm.addEventListener("submit", (e) => {
+shoppingForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (e.target.name.value !== "") {
+  if (e.target.name.value !== '') {
     state.push({
       id: Date.now(),
       item: e.target.name.value,
@@ -13,45 +13,49 @@ shoppingForm.addEventListener("submit", (e) => {
     });
   }
   e.target.reset();
-  list.dispatchEvent(new CustomEvent("listUpdated"));
+  list.dispatchEvent(new CustomEvent('listUpdated'));
 });
 
 function displayList(e) {
   const items = state.map((item) => {
     return `
        <li class="list__item">
-          <input ${item.complete && "checked"}  data-id='${
+          <input " ${item.complete && 'checked'}  data-id='${
       item.id
     }' type="checkbox"/>
-          ${item.item}
+
+         <span style="text-decoration: ${
+           item.complete ? 'line-through' : 'none'
+         }">  ${item.item} </span>
+         
           <span data-id='${item.id}' class="list__item__delete"> &times </span
        </li>
     `;
   });
-  list.innerHTML = items.join("");
+  list.innerHTML = items.join('');
 }
 
 function mirrorToLocalStorage() {
-  localStorage.setItem("items", JSON.stringify(state));
+  localStorage.setItem('items', JSON.stringify(state));
 }
 
-list.addEventListener("listUpdated", displayList);
-list.addEventListener("listUpdated", mirrorToLocalStorage);
+list.addEventListener('listUpdated', displayList);
+list.addEventListener('listUpdated', mirrorToLocalStorage);
 
 function removeItem(id) {
   let newList = state.filter((item) => item.id !== id);
   state = newList;
-  list.dispatchEvent(new CustomEvent("listUpdated"));
+  list.dispatchEvent(new CustomEvent('listUpdated'));
 }
 
 function markAsComplete(id) {
   let itemRef = state.find((item) => item.id === id);
   itemRef.complete = !itemRef.complete;
-  list.dispatchEvent(new CustomEvent("listUpdated"));
+  list.dispatchEvent(new CustomEvent('listUpdated'));
 }
 
-list.addEventListener("click", (e) => {
-  if (e.target.matches("span")) {
+list.addEventListener('click', (e) => {
+  if (e.target.matches('span')) {
     removeItem(Number(e.target.dataset.id));
   }
   if (e.target.matches('[type="checkbox"]')) {
@@ -60,11 +64,11 @@ list.addEventListener("click", (e) => {
 });
 
 function restoreFromLocalStorage() {
-  state = [...JSON.parse(localStorage.getItem("items"))];
-  list.dispatchEvent(new CustomEvent("listUpdated"));
+  state = [...JSON.parse(localStorage.getItem('items'))];
+  list.dispatchEvent(new CustomEvent('listUpdated'));
 }
 restoreFromLocalStorage();
 
 function generateRandom() {
-  return (Number(String(Date.now()).split("").slice(-4).join("")));
+  return Number(String(Date.now()).split('').slice(-4).join(''));
 }
